189eacf172358416581effeab1564568402f85bf
dipakmeher <dipakmeher54@gmail.com>
2022-11-30 14:41:44 -0500

    Added PracDP Folder

---
+++ Day54_11302022/PracDP/knapSack0_1_type/2_knapSack0-1_Memoized.py
@@ -0,0 +1,23 @@
+# 0-1 Knapsack - Memoized
+def knapSack0_1_M(wt, val, W, n):
+    # BC
+    if(n==0 or W==0):
+        return 0
+    if(K[n][W] != -1):
+        return K[n][W]
+    
+    # CD
+    if(wt[n-1] <= W):
+        K[n][W] = max(val[n-1] + knapSack0_1_M(wt,val,W-wt[n-1], n-1), knapSack0_1_M(wt,val,W, n-1))
+    else:
+        K[n][W] = knapSack0_1_M(wt,val,W, n-1)
+    return K[n][W]
+
+# Drivers Code
+val = [60, 100, 120]
+wt = [10, 20, 30]
+n = len(wt)
+W = 50
+
+K = [[-1 for col in range(W+1)] for row in range(n+1)]
+print("Max Profit (KS_ Memoized) = ", knapSack0_1_M(wt, val, W, n))
