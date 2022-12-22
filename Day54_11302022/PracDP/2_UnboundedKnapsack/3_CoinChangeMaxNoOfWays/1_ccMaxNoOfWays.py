d7d292b175dec16e312914508abbfb8bdf90ad03
dipakmeher <dipakmeher54@gmail.com>
2022-12-21 23:44:17 -0800

    Coin Change Max no of ways

---
+++ Day54_11302022/PracDP/2_UnboundedKnapsack/3_CoinChangeMaxNoOfWays/1_ccMaxNoOfWays.py
@@ -0,0 +1,26 @@
+#Coin change: Max number of ways problem
+#Combination of Unbounded Knapsack + Count of Subset sum
+
+
+def coinChagneMaxWays(Arr, n, Sum):
+    for j in range(Sum+1):
+        k[0][j] = 0
+   
+    for i in range(n+1):
+        k[i][0] = 1
+
+    for i in range(1,n+1): #Has to be start with 1,n+1
+        for j in range(1,Sum+1):
+            if(Arr[i-1]<=j):
+                k[i][j] = k[i][j-Arr[i-1]] + k[i-1][j] #i: unbounded knapsack
+            else:
+                k[i][j] = k[i-1][j]
+    return k[n][Sum]
+
+#Driver's Code
+Arr = [1,2,3] # Coin Array
+n = len(Arr)
+Sum = 5
+
+k = [[0 for i in range(Sum+1)] for j in range(n+1)]
+print("Max number of ways to get the sum:",Sum,": with these coins are",coinChagneMaxWays(Arr, n, Sum))
