9d38333f5e647e76a4f9456e4949a924341e5a6e
dipakmeher <dipakmeher54@gmail.com>
2022-12-23 17:48:11 -0800

    Shortest Commnon Supersequence coded

---
+++ Day54_11302022/PracDP/3_LCS/4_ShortestCommonSupersequence_Recursion.py/1_ShortestCommonSupersequence_DP.py
@@ -0,0 +1,20 @@
+# Shortest Common Supersequence LCS
+# Main Function
+def LCSupersequence(s1,s2,n,m):
+    for i in range(n+1):
+        for j in range(m+1):
+            if i == 0 or j == 0:
+                k[i][j] = 0
+            elif(s1[i-1] == s2[j-1]):
+                k[i][j] = 1 + k[i-1][j-1]
+            else:
+                k[i][j] = max(k[i-1][j],k[i][j-1])
+    return n+m - k[n][m]
+# Drivers Code
+s1 = "abcdgh"
+s2 = "abedfr"
+n = len(s1)
+m = len(s2)
+
+k = [[0 for j in range(m+1)] for i in range(n+1)]
+print("The shortest common supersequence for string s1 & s2 is,",LCSupersequence(s1,s2,n,m))
