a107847e1b215678e3f2ee265c25e3a7d03c5d5f
dipakmeher <dipakmeher54@gmail.com>
2022-12-26 00:43:28 -0800

    Palindrome Partitioning Coded

--- Day54_11302022/PracDP/4_MatrixChainMultiplication/2_PallindromePartitioning/3_PP_Memorization_OP.py
+++ Day54_11302022/PracDP/4_MatrixChainMultiplication/2_PallindromePartitioning/3_PP_Memorization_OP.py
@@ -1,4 +1,4 @@
-# Palindrome Partitioning Memorized Bottom-up
+# Palindrome Partitioning Optimized
 import sys
 # function
 def isPalindrome(s1,i,j):
@@ -18,7 +18,16 @@ def PPartitioning(s1,i,j):
     if(dp[i][j] != -1):
         return dp[i][j]
     for k in range(i, j): # If we take till j then it will go till j-1
-        tempAns = PPartitioning(s1, i, k) + PPartitioning(s1, k+1, j) + 1 # 1 is for counting the current partition
+        if(dp[i][k] != -1):
+            left =  dp[i][k]
+        else:
+            left = PPartitioning(s1, i, k)
+        
+        if(dp[k+1][j] != -1):
+            right =  dp[k+1][j]
+        else:
+            right = PPartitioning(s1, k+1, j)
+        tempAns = left + right + 1 # 1 is for counting the current partition
         result = min(result, tempAns)
     dp[i][j] = result
     return result
