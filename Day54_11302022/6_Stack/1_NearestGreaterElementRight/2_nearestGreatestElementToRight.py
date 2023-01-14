4376920d7b45a3f657f4b0f27d9c035e5479bb57
dipakmeher <dipakmeher54@gmail.com>
2023-01-14 14:16:14 -0500

    Next Greater Element for every element in array

---
+++ Day54_11302022/6_Stack/1_NearestGreaterElementRight/2_nearestGreatestElementToRight.py
@@ -0,0 +1,28 @@
+#Next Greatest Element for every array
+#Function
+def nearestGretestElementToRight(arr, n):
+    for i in range(n-1,-1,-1):
+        if(len(stack) == 0):
+            res.append(-1)
+        elif(len(stack)>0 and stack[-1]>arr[i]):
+            res.append(stack[-1])
+        elif(len(stack) > 0 and stack[-1] <= arr[i]):
+            while(len(stack) > 0 and stack[-1]<=arr[i]):
+                stack.pop()
+            if(len(stack) == 0):
+                res.append(-1)
+            else:
+                res.append(stack[-1])
+        stack.append(arr[i])
+
+        
+
+#Driver Code
+stack = []
+res = []
+arr = [1,3,2,4]
+n = len(arr)
+nearestGretestElementToRight(arr, n)
+print(arr)
+res.reverse()
+print(res)
