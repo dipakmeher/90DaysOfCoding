be39c79fc4f0223ebeb09724c6daaa021c4ee434
dipakmeher <dipakmeher54@gmail.com>
2023-01-16 10:55:01 -0500

    Sort the K sorted Array Coded

---
+++ Day54_11302022/6_Heap/2_kthLargestElement/1_kthLargestEle.py
@@ -0,0 +1,12 @@
+#Find the kth smallest element in heap
+import heapq
+
+#Driver Code
+arr = [6, 7, 9, 4, 3, 5, 8, 10, 1]
+k = 3
+heapq.heapify(arr)
+
+print(heapq.nlargest(k,arr))
+
+print(heapq.nlargest(k,arr)[-1])
+
---
+++ Day54_11302022/6_Heap/4_SortKSortedArray/1_sortKSortedArray.py
@@ -0,0 +1,25 @@
+import heapq
+#Function
+def sortKSortedArray(arr,n,k):
+
+    heap = arr[:k+1]
+    heapq.heapify(heap)
+
+    index = 0
+    for remEle in range(k+1,n):
+        arr[index] = heapq.heappop(heap)
+        heapq.heappush(heap,arr[remEle])
+        index+=1
+    
+    while heap:
+        arr[index] = heapq.heappop(heap)
+        index+=1
+
+    print(arr)
+
+#Drive Code
+k = 4
+arr = [6,5,3,2,8,10,9]
+n = len(arr)
+print(arr)
+sortKSortedArray(arr,n,k)
