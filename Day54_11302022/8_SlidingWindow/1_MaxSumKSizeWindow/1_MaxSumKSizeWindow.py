42e2caa6c839e20b9483bdeeb97b8a4c1ef85809
dipakmeher <dipakmeher54@gmail.com>
2023-01-19 09:05:41 -0500

    MaxSumKSizeWindow Coded

---
+++ Day54_11302022/8_SlidingWindow/1_MaxSumKSizeWindow/1_MaxSumKSizeWindow.py
@@ -0,0 +1,25 @@
+# FInd the maximum sum subarray of window size k
+#Function
+def maxSumKSizeWindow(arr,n,k):
+    i = 0
+    j = 0
+    Sum = 0
+    mx = -1
+    while(j<n):
+        Sum = Sum + arr[j]
+        print(Sum)
+        if(j-i+1 < k):
+            j+=1
+        elif(j-i+1 == k):
+            mx = max(mx,Sum)
+            Sum = Sum - arr[i]
+            i+=1
+            j+=1
+    print("Max Sum Window of Size K is ", mx)
+
+#Driver Code
+arr = [ 100, 200, 300, 400]
+n = len(arr)
+k=2
+print(arr)
+maxSumKSizeWindow(arr,n,k)
