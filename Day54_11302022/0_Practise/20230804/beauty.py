2ebe3a41f631e3bafcec2872be7d40ea4071481b
dipakmeher <dipakmeher54@gmail.com>
2023-08-06 23:39:09 -0400

    Need to solve

--- Day54_11302022/0_Practise/20230804/beauty.py
+++ Day54_11302022/0_Practise/20230804/beauty.py
@@ -1,16 +1,16 @@
 #need to solve
 def beautyOfArray(arr, c, n):
-    if(len(arr) == 0):
+    if(len(arr) == 0 or n == 0):
         return c 
 
     #include
-    if(arr[n] == n):
+    if(arr[n-1] == n-1):
         c+=1
-        beautyOfArray(arr, c, n-1)
-        beautyOfArray(arr[:n], c, n)
+    res = max(beautyOfArray(arr, c, n-1),beautyOfArray(arr[:n], c, n-1))
+    print("Result: ",res)
     
 
 arr = [1,3,2,5,4,5,3]
 c = 0
 n = len(arr)
-beautyOfArray(arr,c,n)
+print(beautyOfArray(arr,c,n))
