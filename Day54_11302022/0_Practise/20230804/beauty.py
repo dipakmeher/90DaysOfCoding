5bd0805d191e298479f51c4773d113ab6420ae88
dipakmeher <dipakmeher54@gmail.com>
2023-08-12 16:21:40 -0400

    BeautyOfArray

--- Day54_11302022/0_Practise/20230804/beauty.py
+++ Day54_11302022/0_Practise/20230804/beauty.py
@@ -1,16 +1,20 @@
-#need to solve tomorrow/tomorrow/tomorrow
+# Wrong answer with the commented inputs
 def beautyOfArray(arr, c, n):
-    if(len(arr) == 0 or n == 0):
-        return c 
-
-    #include
-    if(arr[n-1] == n-1):
-        c+=1
-    res = max(beautyOfArray(arr, c, n-1),beautyOfArray(arr[:n], c, n-1))
-    print("Result: ",res)
+    if(n == 0):
+        return 0
+    if(arr[n-1] == n):
+        ans = 1+beautyOfArray(arr, c, n-1)
+        return ans
+    else:
+        arr2 = arr[:]
+        arr2.pop(n-1)
+        ans1 = max(beautyOfArray(arr, c, n-1), beautyOfArray(arr2,c,len(arr2)))
+        return ans1
     
 
-arr = [1,3,2,5,4,5,3]
+# arr = [1,3,2,5,4,5,3]
+arr = [1,3,2,5,4,5]
 c = 0
 n = len(arr)
-print(beautyOfArray(arr,c,n))
+print("Arr",arr)
+print("Count of Bounty: ", beautyOfArray(arr,c,n))
---
+++ Day54_11302022/0_Practise/20230804/test.py
@@ -0,0 +1,5 @@
+arr = [1,3,2,5,4,5,3]
+n = len(arr)
+arr1 = arr[:]
+arr1.pop(n-1)
+print(arr1)
